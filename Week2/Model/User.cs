fe265965c226d74ae26410a5c42307bafdcff4dd
emirxmertoglu <emir.mertoglu.97@gmail.com>
2021-12-05 09:43:00 +0300

    🚀 coded authorization

---
+++ Week2/Model/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class User : IUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Adinizi giriniz!")]
+        [MaxLength(100, ErrorMessage = "Adiniz max 100 karakter olabilir!")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email adresinizi giriniz!")]
+        [EmailAddress(ErrorMessage = "Gecerli bir mail adresi giriniz!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Sifrenizi giriniz!")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Kullanici rolunuzu giriniz!")]
+        public string Role { get; set; }
+    }
+}
---
+++ Week2/Web/Controllers/LoginFilter.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Model;
+using Microsoft.AspNetCore.Routing;
+
+namespace Web.Controllers
+{
+    public class LoginFilter : Attribute, IActionFilter
+    {
+        // creating some dummy user data
+        public static List<User> users = new List<User>
+        {
+            new User
+            {
+                Email = "emir.mertoglu.97@gmail.com",
+                FullName = "Emir Mertoglu",
+                Id = 1,
+                Password = "emir123",
+                Role = "Admin"
+            },
+            new User
+            {
+                Email = "test@gmail.com",
+                FullName = "Test User",
+                Id = 2,
+                Password = "test00",
+                Role = "Manager"
+            },
+            new User
+            {
+                Email = "test2@gmail.com",
+                FullName = "Test User2",
+                Id = 3,
+                Password = "test11",
+                Role = "Tester"
+            }
+        };
+
+        private string _role;
+        public LoginFilter(string role)
+        {
+            _role = role;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {}
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // checking any user has the role
+            if(users.Any(x => x.Role == _role))
+            {
+                return;
+            }
+            else
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Home" }, { "action", "Error" } });
+            }
+        }
+    }
+}
